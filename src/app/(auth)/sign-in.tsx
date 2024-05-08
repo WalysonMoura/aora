7102b6f459973c77138c722d36873b6ccab1cd69
{ Walyson } <walysonmoura222@gmail.com>
2024-05-08 14:16:41 +0000

    new

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignIn() {
   return (
     <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View>
+          <Image></Image>
+        </View>
+      </ScrollView>
       <View>
         <Text className="font-psemibold mt-10 text-2xl font-semibold text-white">
           Sign Up to Aora
